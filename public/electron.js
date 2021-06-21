f06ca76b6637d9fd7ad0831793cfa09f4d2d2989
xihnik <shkadinskiy98@gmail.com>
2021-06-21 21:57:32 +0300

    feat: updated configuration of electron

--- public/electron.js
+++ public/electron.js
@@ -1,35 +1,95 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, globalShortcut } = require('electron')
 const path = require('path')
+const axios = require('axios').default
 const isDev = require('electron-is-dev')
 
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
+    center: true,
     width: 800,
     height: 600,
+    backgroundColor: '#313131',
+    minHeight: 600,
+    minWidth: 300,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false
     },
     fullscreen: false,
     autoHideMenuBar: true
   })
+  // mainWindow.setMenu(null);
 
   const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
 
-  // and load the index.html of the app.
-  // mainWindow.loadFile('index.html')
   mainWindow.loadURL(startURL);
+
+  globalShortcut.register('Alt+A', () => {
+    if (mainWindow.webContents.getURL() != (isDev ? 'http://localhost:3000/#/admin' : `file://${path.join(__dirname, '../build/index.html#/admin')}`))
+      mainWindow.loadURL(isDev ? 'http://localhost:3000/#/admin' : `file://${path.join(__dirname, '../build/index.html#/admin')}`);
+  })
+
+  globalShortcut.register('F11', () => {
+    if (!mainWindow.isFullScreen())
+      mainWindow.setFullScreen(true);
+    else
+      mainWindow.setFullScreen(false);
+  })
+
+  globalShortcut.register('F10', () => {
+    if (!mainWindow.isMaximized())
+      mainWindow.maximize()
+    else
+      mainWindow.unmaximize()
+  })
+
+  globalShortcut.register('F9', () => {
+    mainWindow.minimize()
+  })
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
 
+async function isVersionLast(localVersion) {
+  try {
+    let versionOnServer = await new Promise((resolve, reject) => {
+      axios.get('http://localhost:80/root/version.php').then((result) => {
+        resolve(result.data);
+      }).catch((e) => {
+        reject(e);
+      })
+    })
+
+    if (versionOnServer > localVersion) {
+      const { dialog } = require('electron');
+      dialog.showErrorBox('Обновитесь!', 'Вышла новая версия')
+      return false;
+    }
+
+    return true;
+  }
+  catch (error) {
+    if (error.toJSON().message !== 'Network Error' && error.toJSON().code !== 'ECONNREFUSED')
+      return false;
+
+    const { dialog } = require('electron');
+    dialog.showErrorBox('Отсутствует подлючение к серверу', 'Проверьте свое интернет соединение');
+    return false;
+  }
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+app.whenReady().then(async () => {
+  if (await isVersionLast('0.0.1'))
+    createWindow();
+  else
+    app.quit()
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
